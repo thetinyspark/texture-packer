eff5e1014ccfa443d649411dd4aaf2e5f891b473
thetinyspark <contact@the-tiny-spark.com>
2022-08-07 05:27:43 +0200

    update version

--- dist/core/service/UserArgsService.js
+++ dist/core/service/UserArgsService.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var commander_1 = require("commander");
 var UserArgsService = /** @class */ (function () {
     function UserArgsService() {
-        commander_1.program.version("2.0.0");
+        commander_1.program.version("1.11.0");
         commander_1.program.option('--dir <type>', 'the texture source directory', './');
         commander_1.program.option('--size <number>', 'the output atlas size', '2048');
         commander_1.program.option('--output <type>', 'the output dir', './');
--- lib/core/service/UserArgsService.ts
+++ lib/core/service/UserArgsService.ts
@@ -3,7 +3,7 @@ import IUserArgsService from "./IUserArgsService";
 
 export default class UserArgsService implements IUserArgsService{
     constructor(){
-        program.version("2.0.0"); 
+        program.version("1.11.0"); 
         program.option('--dir <type>', 'the texture source directory', './');
         program.option('--size <number>', 'the output atlas size', '2048');
         program.option('--output <type>', 'the output dir', './');
